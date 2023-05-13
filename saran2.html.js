09eb7283988569a24d8594d9fae0c8051a8a5e74
Chiyansaran <133480771+Chiyansaran@users.noreply.github.com>
2023-05-13 14:29:05 +0530

    Add files via upload

---
+++ saran2.html.js
@@ -0,0 +1,18 @@
+// array of movies with their corresponding theaters and showtimes
+const movieData = [
+  {
+    movieName: "Avengers: Endgame",
+    theaters: ["AMC Theatres", "Regal Cinemas"],
+    showtimes: ["2023-05-13T10:30", "2023-05-13T14:00", "2023-05-13T18:00"]
+  },
+  {
+    movieName: "The Lion King",
+    theaters: ["Cinemark Theatres", "Cineplex"],
+    showtimes: ["2023-05-13T11:00", "2023-05-13T15:30", "2023-05-13T19:30"]
+  },
+  {
+    movieName: "Joker",
+    theaters: ["AMC Theatres", "Cineplex"],
+    showtimes: ["2023-05-13T12:00", "2023-05-13T16:30", "2023-05-13T20:30"]
+  },
+  {
